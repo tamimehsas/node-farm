ce8a38e0e7d2031316ff8dacbb8a2f91cdd08255
Tamim Ehsas <tamimehsas99@gmail.com>
2022-01-04 14:48:43 +0430

    Add the replaceTemplate function to treplace the template placeholders with data

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+    return output;
+
+}
+
+
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
@@ -14,8 +32,9 @@ const server = http.createServer((req, res) => {
     // OVERVIEW
     if (pathName === '/' || pathName === '/overview') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el));
-        res.end(tempOverview);
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%TEMPLATE-CARDS%}',cardsHtml);
+        res.end(output);
 
         // PRODUCT
     } else if (pathName === '/product') {
